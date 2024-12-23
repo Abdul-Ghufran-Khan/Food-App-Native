a7c9a84c60ceeb2fb3e6526524ac5e2de528319c
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-12-23 14:25:29 +0500

    Commit

--- app/home.tsx
+++ app/home.tsx
@@ -100,6 +100,12 @@ const home = () => {
             price: "Rs. 850",
             rating: "4.5",
           },
+          {
+            image: "https://images.unsplash.com/photo-1694579740719-0e601c5d2437?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            name: "Afghani boti",
+            price: "Rs. 750",
+            rating: "4.5",
+          },
           ]}
           horizontal
           showsHorizontalScrollIndicator={false}
