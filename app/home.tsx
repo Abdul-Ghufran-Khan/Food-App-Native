add311821253959443995acde7a2a5e661ac4340
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2025-02-20 22:26:11 +0500

    Commit

--- app/home.tsx
+++ app/home.tsx
@@ -37,7 +37,7 @@ const home = () => {
         </TouchableOpacity>
       </View>
 
-      <View className='px-5 mt-3'>
+      <View className='px-5 mt-4'>
         <FlatList
           data={[
             "Most Viewed",
